d636888e67ea24afcc57a5eedb6fec1b0d39d31d
lewisd32 <github@lewisd.com>
2015-02-09 12:57:03 -0800

    Moving some scheduling info into a union to save a couple bytes.  Adding functions to allow rescheduling of other tasks. Fixing a signed subtraction vs comparison bug.

--- Tasker.cpp
+++ Tasker.cpp
@@ -5,7 +5,13 @@
 const uint32_t MAX_LONG = 4294967295;
 const int32_t MAX_SIGNED_LONG = MAX_LONG/2-1;
 
-Task* currentTask;
+#define never(now) ((uint32_t)now + MAX_SIGNED_LONG)
+
+struct CurrentTaskInfo {
+  Task* task;
+  uint32_t scheduledExecutionTime;
+} currentTaskInfo;
+
 
 void initTasks(struct Tasks& tasks, Task taskStructs[], uint8_t maxTasks) {
   tasks.tasks = taskStructs;
@@ -16,47 +22,23 @@ void initTasks(struct Tasks& tasks, Task taskStructs[], uint8_t maxTasks) {
   }
 }
 
-void execTask(struct Task& task) {
-  currentTask = &task;
-  // Save the timestamp of when it executes for later.
-  uint32_t taskExecutedTimeUs = micros();
-
-  // If it's not a periodic task, don't execute it again without rescheduling.
-  if (task.schedule.type != PERIODIC) {
-    task.schedule.type = NONE;
-  }
-
-  // Execute the task, passing in the task struct and it's state.
-  task.func(task, task.state);
-  uint32_t taskDurationUs = micros() - taskExecutedTimeUs;
-  // Record the time it ran, and the time it was scheduled to run.
-  task.lastExecutedTimeUs = taskExecutedTimeUs;
-  task.lastScheduledTimeUs = task.schedule.requestedNextExecutionTimeUs;
-  task.maxDurationUs = max(task.maxDurationUs, taskDurationUs);
-
-  // If it's a periodic task, set up the next scheduled execution.
-  if (task.schedule.type == PERIODIC) {
-    uint32_t period = task.schedule.period;
-    task.schedule.requestedNextExecutionTimeUs = task.lastScheduledTimeUs + period;
-  }
-  currentTask = NULL;
-}
-
 int32_t getTimeUntilExecution(struct Task& task, uint32_t now) {
   TaskSchedule& schedule = task.schedule;
   switch (schedule.type) {
     case ONCE:
     case SLEEP:
+      return schedule.once.requestedNextExecutionTimeUs - now;
+      break;
     case PERIODIC:
-      return schedule.requestedNextExecutionTimeUs - now;
+      return schedule.periodic.requestedNextExecutionTimeUs - now;
       break;
     case UNTIL:
-      if (*schedule.whenVar) {
+      if (*schedule.when.whenVar) {
         return 0;
       }
       break;
     case WHEN:
-      if (!*schedule.whenVar) {
+      if (!*schedule.when.whenVar) {
         return 0;
       }
       break;
@@ -66,37 +48,106 @@ int32_t getTimeUntilExecution(struct Task& task, uint32_t now) {
       serWarn("Unknown task schedule type ");
       serWarnln(schedule.type);
   }
-  serTrace("returning ");
-  serTraceln(MAX_LONG);
-  return MAX_SIGNED_LONG;
+  serTrace("returning never (");
+  serTrace(MAX_LONG);
+  serTrace(")");
+  return never(now);
 }
 
-Task* findNextTask(struct Tasks& tasks) {
+Task* findNextTask(struct Tasks& tasks, uint32_t now) {
   // TODO use priority
   Task* selectedTask = NULL;
-  int32_t timeUntilExec = MAX_SIGNED_LONG;
-  uint32_t now = micros();
+  int32_t timeUntilExec = never(now);
+  int32_t neverUs = never(now);
   serDebug("timeUntilExec ");
-  serDebugln(timeUntilExec);
+  if (timeUntilExec == neverUs) {
+    serDebug("never (");
+    serDebug(timeUntilExec);
+    serDebugln(")");
+  } else {
+    serDebugln(timeUntilExec);
+  }
   for (uint8_t i = 0; i < tasks.maxTasks; ++i) {
     Task& task = tasks.tasks[i];
     int32_t taskTimeUntilExec = getTimeUntilExecution(task, now);
-    serDebug("taskTimeUntilExec ");
-    serDebugln(taskTimeUntilExec);
-    if (taskTimeUntilExec < timeUntilExec) {
-      serDebug("selecting ");
+    serDebug(i);
+    serDebug(" taskTimeUntilExec ");
+    if (taskTimeUntilExec == neverUs) {
+      serDebug("never (");
+      serDebug(taskTimeUntilExec);
+      serDebugln(")");
+    } else {
+      serDebugln(taskTimeUntilExec);
+    }
+    if (taskTimeUntilExec != neverUs &&
+        (taskTimeUntilExec - timeUntilExec < 0 || timeUntilExec == neverUs)) {
+      serDebug(i);
+      serDebug(" selecting ");
       serDebugln(i);
       selectedTask = &task;
       timeUntilExec = taskTimeUntilExec;
+      currentTaskInfo.scheduledExecutionTime = now + taskTimeUntilExec;
     }
   }
+  currentTaskInfo.task = selectedTask;
   return selectedTask;
 }
 
+void execTask(struct Task& task) {
+  // Set currentTask so that the various sleep functions can use it.
+  currentTaskInfo.task = &task;
+
+  // Save the timestamp of when it executes for later.
+  uint32_t taskExecutedTimeUs = micros();
+
+  ScheduleType scheduleType = task.schedule.type;
+  // If it's not a periodic task, don't execute it again without rescheduling.
+  if (scheduleType != PERIODIC) {
+    task.schedule.type = NONE;
+  }
+
+  // Execute the task, passing in the task struct and it's state.
+  task.func(task, task.state);
+
+  // Calculate how long the task took.
+  uint32_t taskDurationUs = micros() - taskExecutedTimeUs;
+  // Record the time that the task actually ran at.
+  task.lastExecutedTimeUs = taskExecutedTimeUs;
+
+  // For some task types, record the time it had been scheduled to run at.
+  // It's only safe to write to the schedule struct if the type (and hence the
+  // union) didn't change, or was set to NONE above, and left unchanged.
+  if ((scheduleType == ONCE ||
+      scheduleType == SLEEP) &&
+      (task.schedule.type == NONE ||
+       task.schedule.type == ONCE ||
+       task.schedule.type == SLEEP)) {
+    task.schedule.once.lastScheduledTimeUs = task.schedule.once.requestedNextExecutionTimeUs;
+  }
+  if (scheduleType == PERIODIC &&
+      (task.schedule.type == NONE ||
+       task.schedule.type == PERIODIC)) {
+    task.schedule.periodic.lastScheduledTimeUs = task.schedule.periodic.requestedNextExecutionTimeUs;
+  }
+
+  // Record the maximum length of time this task has taken.
+  task.maxDurationUs = max(task.maxDurationUs, taskDurationUs);
+
+  // If it's a periodic task, set up the next scheduled execution.
+  if (task.schedule.type == PERIODIC) {
+    uint32_t period = task.schedule.periodic.period;
+    task.schedule.periodic.requestedNextExecutionTimeUs = task.schedule.periodic.lastScheduledTimeUs + period;
+  }
+
+  // Clear the current task, so that misplaced sleep calls don't mess with things.
+  currentTaskInfo.task = NULL;
+}
+
 void execNextTask(struct Tasks& tasks) {
-  Task* selected = findNextTask(tasks);
-  if (selected) {
-    int32_t scheduledTimeUs = selected->schedule.requestedNextExecutionTimeUs;
+  uint32_t now = micros();
+  Task* selectedTask = findNextTask(tasks, now);
+  if (selectedTask) {
+    int32_t scheduledTimeUs = currentTaskInfo.scheduledExecutionTime;
     serInfo("Scheduled at ");
     serInfoln(scheduledTimeUs);
     serInfo("Now ");
@@ -106,12 +157,16 @@ void execNextTask(struct Tasks& tasks) {
       serTrace(".");
     }
     serInfoln("Executing");
-    execTask(*selected);
+    execTask(*selectedTask);
     serInfoln("done");
   } else {
     serDebugln("No task to execute");
   }
 }
+
+/***************************************************************
+ * Scheduling
+ ***************************************************************/
 
 Task* findEmptyTaskSlot(struct Tasks& tasks) {
   for (uint8_t i = 0; i < tasks.maxTasks; ++i) {
@@ -123,45 +178,57 @@ Task* findEmptyTaskSlot(struct Tasks& tasks) {
   return NULL;
 }
 
-/***************************************************************
- * Scheduling
- ***************************************************************/
-
 Task* _scheduleOnce(struct Tasks& tasks, TaskFunc func, void* state, uint32_t scheduledExecutionTimeUs) {
   Task& task = *findEmptyTaskSlot(tasks);
   task.func = func;
   task.state = state;
-  task.schedule.type = ONCE;
-  task.schedule.requestedNextExecutionTimeUs = scheduledExecutionTimeUs;
+  scheduleTaskOnce(&task, scheduledExecutionTimeUs);
   return &task;
+}
+
+void scheduleTaskOnce(struct Task* task, uint32_t scheduledExecutionTimeUs) {
+  task->schedule.type = ONCE;
+  task->schedule.once.requestedNextExecutionTimeUs = scheduledExecutionTimeUs;
 }
 
 Task* _schedulePeriodic(struct Tasks& tasks, TaskFunc func, void* state, uint32_t scheduledExecutionTimeUs, uint32_t intervalUs) {
   Task& task = *findEmptyTaskSlot(tasks);
   task.func = func;
   task.state = state;
-  task.schedule.type = PERIODIC;
-  task.schedule.requestedNextExecutionTimeUs = scheduledExecutionTimeUs;
-  task.schedule.period = intervalUs;
+  scheduleTaskPeriodic(&task, scheduledExecutionTimeUs, intervalUs);
   return &task;
+}
+
+void scheduleTaskPeriodic(struct Task* task, int32_t scheduledExecutionTimeUs, uint32_t intervalUs) {
+  task->schedule.type = PERIODIC;
+  task->schedule.periodic.requestedNextExecutionTimeUs = scheduledExecutionTimeUs;
+  task->schedule.periodic.period = intervalUs;
 }
 
 Task* _scheduleWhen(struct Tasks& tasks, TaskFunc func, void* state, bool& whenVar) {
   Task& task = *findEmptyTaskSlot(tasks);
   task.func = func;
   task.state = state;
-  task.schedule.type = WHEN;
-  task.schedule.whenVar = &whenVar;
+  scheduleTaskWhen(&task, whenVar);
   return &task;
+}
+
+void scheduleTaskWhen(struct Task* task, bool& whenVar) {
+  task->schedule.type = WHEN;
+  task->schedule.when.whenVar = &whenVar;
 }
 
 Task* _scheduleUntil(struct Tasks& tasks, TaskFunc func, void* state, bool& untilVar) {
   Task& task = *findEmptyTaskSlot(tasks);
   task.func = func;
   task.state = state;
-  task.schedule.type = UNTIL;
-  task.schedule.whenVar = &untilVar;
+  scheduleTaskUntil(&task, untilVar);
   return &task;
+}
+
+void scheduleTaskUntil(struct Task* task, bool& untilVar) {
+  task->schedule.type = UNTIL;
+  task->schedule.when.whenVar = &untilVar;
 }
 
 /***************************************************************
@@ -170,41 +237,41 @@ Task* _scheduleUntil(struct Tasks& tasks, TaskFunc func, void* state, bool& unti
 
 
 void sleepMillis(uint16_t milliseconds) {
-  if (currentTask) {
+  if (currentTaskInfo.task) {
     uint32_t now = micros();
-    TaskSchedule& schedule = currentTask->schedule;
+    TaskSchedule& schedule = currentTaskInfo.task->schedule;
     schedule.type = SLEEP;
-    schedule.requestedNextExecutionTimeUs = now + (uint32_t)milliseconds*1000;
+    schedule.once.requestedNextExecutionTimeUs = now + (uint32_t)milliseconds*1000;
   } else {
     serWarnln("Cannot call sleepMillis outside of a task");
   }
 }
 
 void syncSleepMillis(uint16_t milliseconds) {
-  if (currentTask) {
-    TaskSchedule& schedule = currentTask->schedule;
+  if (currentTaskInfo.task) {
+    TaskSchedule& schedule = currentTaskInfo.task->schedule;
     schedule.type = SLEEP;
-    schedule.requestedNextExecutionTimeUs += (uint32_t)milliseconds*1000;
+    schedule.once.requestedNextExecutionTimeUs += (uint32_t)milliseconds*1000;
   } else {
     serWarnln("Cannot call syncSleepMillis outside of a task");
   }
 }
 
 void sleepUntil(bool& untilVar) {
-  if (currentTask) {
-    TaskSchedule& schedule = currentTask->schedule;
+  if (currentTaskInfo.task) {
+    TaskSchedule& schedule = currentTaskInfo.task->schedule;
     schedule.type = UNTIL;
-    schedule.whenVar = &untilVar;
+    schedule.when.whenVar = &untilVar;
   } else {
     serWarnln("Cannot call sleepUntil outside of a task");
   }
 }
 
 void sleepWhen(bool& whenVar) {
-  if (currentTask) {
-    TaskSchedule& schedule = currentTask->schedule;
+  if (currentTaskInfo.task) {
+    TaskSchedule& schedule = currentTaskInfo.task->schedule;
     schedule.type = WHEN;
-    schedule.whenVar = &whenVar;
+    schedule.when.whenVar = &whenVar;
   } else {
     serWarnln("Cannot call sleepWhen outside of a task");
   }
--- Tasker.h
+++ Tasker.h
@@ -3,6 +3,8 @@
 
 #include <Arduino.h>
 
+// Scheduling is all done in microseconds, so multiplying by this constant
+// gives milliseconds, for where that's more convenient.
 const uint32_t MS = 1000;
 
 // Forward declaration of Task to allow TaskFunc and Task reference each other.
@@ -21,21 +23,37 @@ typedef void (*TaskFunc)(Task &Task, void *state);
  */
 enum ScheduleType { NONE, ONCE, SLEEP, PERIODIC, WHEN, UNTIL };
 
+struct PeriodicScheduleData {
+  uint32_t requestedNextExecutionTimeUs;
+  uint32_t lastScheduledTimeUs;
+  uint32_t period;
+};
+
+struct OnceScheduleData {
+  uint32_t requestedNextExecutionTimeUs;
+  uint32_t lastScheduledTimeUs;
+};
+
+struct WhenScheduleData {
+  bool* whenVar;
+};
+
 struct TaskSchedule {
   ScheduleType type;
-  uint32_t requestedNextExecutionTimeUs;
-  uint32_t period;
-  bool* whenVar;
+  union {
+    PeriodicScheduleData periodic;
+    OnceScheduleData once;
+    WhenScheduleData when;
+  };
 };
 
 struct Task {
   uint8_t priority;
-  uint16_t maxDurationUs;
-  uint32_t lastScheduledTimeUs;
+  uint32_t maxDurationUs;
   uint32_t lastExecutedTimeUs;
   TaskSchedule schedule;
-  void* state;
   TaskFunc func;
+  void* state;
 };
 
 struct Tasks {
@@ -49,17 +67,35 @@ void initTasks(struct Tasks& tasks, Task taskStructs[], uint8_t maxTasks);
 
 void execNextTask(struct Tasks& tasks);
 
+// ***** ScheduleOnce *********************
+
 #define scheduleOnce(tasks, func, state, us) (_scheduleOnce(tasks, (TaskFunc)func, (void*)&state, us))
 Task* _scheduleOnce(struct Tasks& tasks, TaskFunc func, void* state, uint32_t scheduledExecutionTimeUs);
+
+void scheduleTaskOnce(struct Task* task, uint32_t scheduledExecutionTimeUs);
+
+// ***** SchedulePeriodic *********************
 
 #define schedulePeriodic(tasks, func, state, first, interval) (_schedulePeriodic(tasks, (TaskFunc)func, (void*)&state, first, interval))
 Task* _schedulePeriodic(struct Tasks& tasks, TaskFunc func, void* state, uint32_t scheduledExecutionTimeUs, uint32_t intervalUs);
 
+void scheduleTaskPeriodic(struct Task* task, int32_t scheduledExecutionTimeUs, uint32_t intervalUs);
+
+// ***** ScheduleWhen *********************
+
 #define scheduleWhen(tasks, func, state, whenVar) (_scheduleWhen(tasks, (TaskFunc)func, (void*)&state, whenVar))
 Task* _scheduleWhen(struct Tasks& tasks, TaskFunc func, void* state, bool& whenVar);
 
+void scheduleTaskWhen(struct Task* task, bool& whenVar);
+
+// ***** ScheduleUntil *********************
+
 #define scheduleUntil(tasks, func, state, whenVar) (_scheduleUntil(tasks, (TaskFunc)func, (void*)&state, untilVar))
 Task* _scheduleUntil(struct Tasks& tasks, TaskFunc func, void* state, bool& untilVar);
+
+void scheduleTaskUntil(struct Task* task, bool& untilVar);
+
+// ***** Sleep *********************
 
 void sleepMillis(uint16_t milliseconds);
 
